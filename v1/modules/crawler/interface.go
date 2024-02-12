970d9c4471c080ab404d45189aff682d7d66e708
Kenneth Tang <kenneth@nssh.onmicrosoft.com>
2024-02-12 23:33:27 +0800

    Update crawler interface

--- v1/modules/crawler/interface.go
+++ v1/modules/crawler/interface.go
@@ -1,20 +1,12 @@
 package crawler
 
-import (
-	"time"
-
-	"github.com/ChengYen-Tang/binance-crawler/models"
-)
-
 type IClient interface {
 }
 
 // IGet is an interface for getting data from the binance api
 type IGet interface {
-	// New creates a new instance of IGet
-	New(*IClient, *chan models.IWorkItem) IGet
 	// GetToNow gets data from the binance api from the start time to the current time
-	GetToNow(time.Time)
+	GetToNow(startTime *int64)
 	// Get gets data from the binance api from the start time to the end time
-	Get(time.Time, time.Time)
+	Get(startTime *int64, endTime *int64)
 }
