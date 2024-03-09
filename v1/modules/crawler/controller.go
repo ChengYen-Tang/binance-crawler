4a252f7f3fec443045806c4a57abd2ec9d46687a
Kenneth Tang <kenneth.tang@asmpt.com>
2024-03-09 22:51:25 +0800

    Semi-finished controller

--- v1/modules/crawler/controller.go
+++ v1/modules/crawler/controller.go
@@ -1,27 +1,143 @@
 package crawler
 
 import (
+	"context"
+
 	"github.com/ChengYen-Tang/binance-crawler/factory"
-	models "github.com/ChengYen-Tang/binance-crawler/models/interface"
+	models "github.com/ChengYen-Tang/binance-crawler/models"
+	iModel "github.com/ChengYen-Tang/binance-crawler/models/interface"
+	"github.com/ChengYen-Tang/binance-crawler/models/parameter"
 	crawler "github.com/ChengYen-Tang/binance-crawler/modules/crawler/interface"
+	"github.com/ChengYen-Tang/binance-crawler/modules/database"
 )
 
 // Controller is a struct for controlling the crawler
 type Controller struct {
-	client  *crawler.IClient
-	channel *chan models.IWorkItem
-	factory *factory.WorkItemFactory
-	getters *[]crawler.IGet
+	dbEndPoint *database.DBEndpoints
+	client     *crawler.IClient
+	channel    *chan iModel.IWorkItem
+	factory    *factory.WorkItemFactory
+	getters    *[]crawler.IGet
+	params     *parameter.ControllerParams
 }
 
 // New creates a new instance of Controller
-func NewController(client *crawler.IClient, channel *chan models.IWorkItem, factory *factory.WorkItemFactory) *Controller {
+func NewController(dbEndPoint *database.DBEndpoints, client *crawler.IClient, channel *chan iModel.IWorkItem, factory *factory.WorkItemFactory, params *parameter.ControllerParams) *Controller {
 	getters := &[]crawler.IGet{}
 
 	return &Controller{
-		client:  client,
-		channel: channel,
-		factory: factory,
-		getters: getters,
+		dbEndPoint: dbEndPoint,
+		client:     client,
+		channel:    channel,
+		factory:    factory,
+		getters:    getters,
+		params:     params,
 	}
+}
+
+func (controller *Controller) Run(ctx context.Context) {
+	controller.check(ctx)
+}
+
+func (controller *Controller) check(ctx context.Context) error {
+	apiName := models.SpotKline
+	for _, symbol := range controller.params.Symbols {
+		isTableExist, err := controller.dbEndPoint.IsTableExist(&apiName, &symbol, ctx)
+		if err != nil {
+			return err
+		}
+
+		var (
+			firstTime *int64
+			lastTime  *int64
+		)
+
+		if isTableExist {
+			firstTime, lastTime, err = controller.dbEndPoint.GetKlineTimeRange(&apiName, &symbol, ctx)
+		} else {
+			err = controller.dbEndPoint.CreateKlineTable(&apiName, &symbol, ctx)
+			startTime := controller.params.StartTime.UnixMilli()
+			firstTime = &startTime
+		}
+		if err != nil {
+			return err
+		}
+	}
+	apiName = models.USDFuturesKline
+	for _, symbol := range controller.params.Symbols {
+		isTableExist, err := controller.dbEndPoint.IsTableExist(&apiName, &symbol, ctx)
+		if err != nil {
+			return err
+		}
+
+		var (
+			firstTime *int64
+			lastTime  *int64
+		)
+
+		if isTableExist {
+			firstTime, lastTime, err = controller.dbEndPoint.GetKlineTimeRange(&apiName, &symbol, ctx)
+		} else {
+			err = controller.dbEndPoint.CreateKlineTable(&apiName, &symbol, ctx)
+			startTime := controller.params.StartTime.UnixMilli()
+			firstTime = &startTime
+		}
+		if err != nil {
+			return err
+		}
+	}
+	apiName = models.USDFuturesPremiumIndexKline
+	for _, symbol := range controller.params.Symbols {
+		isTableExist, err := controller.dbEndPoint.IsTableExist(&apiName, &symbol, ctx)
+		if err != nil {
+			return err
+		}
+
+		var (
+			firstTime *int64
+			lastTime  *int64
+		)
+
+		if isTableExist {
+			firstTime, lastTime, err = controller.dbEndPoint.GetKlineTimeRange(&apiName, &symbol, ctx)
+			if err != nil {
+				return err
+			}
+		} else {
+			err = controller.dbEndPoint.CreateKlineTable(&apiName, &symbol, ctx)
+			startTime := controller.params.StartTime.UnixMilli()
+			firstTime = &startTime
+		}
+		if err != nil {
+			return err
+		}
+	}
+	apiName = models.USDFuturesFundingRate
+	for _, symbol := range controller.params.Symbols {
+		isTableExist, err := controller.dbEndPoint.IsTableExist(&apiName, &symbol, ctx)
+		if err != nil {
+			return err
+		}
+
+		var (
+			firstTime *int64
+			lastTime  *int64
+		)
+
+		if isTableExist {
+			firstTime, lastTime, err = controller.dbEndPoint.GetFundingRateTimeRange(&apiName, &symbol, ctx)
+			if err != nil {
+				return err
+			}
+		} else {
+			err = controller.dbEndPoint.CreateFundingRateTable(&apiName, &symbol, ctx)
+			startTime := controller.params.StartTime.UnixMilli()
+			firstTime = &startTime
+		}
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
